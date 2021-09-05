0938ec8600dfea210c7ecd2d34c661bfa4b1c374
Yuta Kohno <y.kohno1215@gmial.com>
2021-09-06 01:25:33 +0900

    add some tokens

---
+++ src/token.rs
@@ -0,0 +1,206 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Token {
+    PLUS,
+    ASSIGN,
+    SEMICOLON,
+    LPAREN,
+    RPAREN,
+    COMMA,
+    LBRACE,
+    RBRACE,
+    LET,
+    FUNCTION,
+
+    IDENT(String),
+    INT(i32),
+    EOF,
+}
+
+pub struct Lexer {
+    input: Vec<char>,
+    pos: usize,
+}
+
+impl Lexer {
+    pub fn new(input: &str) -> Self {
+        let chars: Vec<char> = input.chars().collect();
+        Lexer {
+            input: chars,
+            pos: 0,
+        }
+    }
+    fn cur(&self) -> char {
+        if self.pos == self.input.len() {
+            '\u{0}'
+        } else {
+            self.input[self.pos]
+        }
+    }
+
+    fn peek(&self) -> char {
+        if self.pos + 1 >= self.input.len() {
+            '\u{0}'
+        } else {
+            self.input[self.pos + 1]
+        }
+    }
+
+    fn next(&mut self) {
+        if self.pos == self.input.len() {
+            return;
+        }
+        self.pos += 1;
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        while self.cur().is_whitespace() {
+            self.next();
+        }
+        if self.at_eof() {
+            return Token::EOF;
+        }
+        if is_letter(self.cur()) {
+            let ident = self.read_ident();
+            return match &*ident {
+                "fn" => Token::FUNCTION,
+                "let" => Token::LET,
+                _ => Token::IDENT(ident),
+            };
+        }
+        if self.cur().is_digit(10) {
+            return Token::INT(self.read_number());
+        }
+        let tok = match self.cur() {
+            '=' => Token::ASSIGN,
+            ';' => Token::SEMICOLON,
+            '(' => Token::LPAREN,
+            ')' => Token::RPAREN,
+            '{' => Token::LBRACE,
+            '}' => Token::RBRACE,
+            ',' => Token::COMMA,
+            '+' => Token::PLUS,
+            x => unreachable!(format!("unreachable: {}", x)),
+        };
+        self.next();
+        tok
+    }
+
+    fn read_ident(&mut self) -> String {
+        if !is_letter(self.cur()) {
+            panic!("initial char is not a letter");
+        }
+        let l = self.pos;
+        while is_letter(self.cur()) {
+            self.next();
+        }
+        self.input[l..self.pos].into_iter().collect()
+    }
+
+    fn read_number(&mut self) -> i32 {
+        if !self.cur().is_digit(10) {
+            panic!("initial char is not a digit");
+        }
+        let l = self.pos;
+        while self.cur().is_digit(10) {
+            self.next();
+        }
+        self.input[l..self.pos]
+            .into_iter()
+            .collect::<String>()
+            .parse()
+            .unwrap() // 長すぎると死ぬ気がする
+    }
+
+    pub fn at_eof(&self) -> bool {
+        self.pos == self.input.len()
+    }
+}
+
+fn is_letter(c: char) -> bool {
+    if 'a' <= c && c <= 'z' {
+        return true;
+    }
+    if 'A' <= c && c <= 'Z' {
+        return true;
+    }
+    if c == '_' {
+        return true;
+    }
+    false
+}
+
+#[test]
+fn test_is_letter() {
+    assert!(is_letter('c'));
+    assert!(is_letter('A'));
+    assert!(is_letter('X'));
+    assert!(is_letter('_'));
+    assert!(!is_letter('='));
+    assert!(!is_letter('{'));
+    assert!(!is_letter('1'));
+}
+
+
+#[test]
+fn test_lexer() {
+    let input = "
+    let ten = 10;
+    let five = 5;
+
+    let add = fn(x, y) {
+        x+y;
+    };
+    let result = add(x, y);
+    ";
+    let mut lexer = Lexer::new(input);
+    let mut tokens = Vec::new();
+    while !lexer.at_eof() {
+        let tok = lexer.next_token();
+        tokens.push(tok);
+    }
+    dbg!(&tokens);
+    let expected = [
+        Token::LET,
+        Token::IDENT("ten".to_string()),
+        Token::ASSIGN,
+        Token::INT(10),
+        Token::SEMICOLON,
+        Token::LET,
+        Token::IDENT("five".to_string()),
+        Token::ASSIGN,
+        Token::INT(5),
+        Token::SEMICOLON,
+        Token::LET,
+        Token::IDENT("add".to_string()),
+        Token::ASSIGN,
+        Token::FUNCTION,
+        Token::LPAREN,
+        Token::IDENT("x".to_string()),
+        Token::COMMA,
+        Token::IDENT("y".to_string()),
+        Token::RPAREN,
+        Token::LBRACE,
+        Token::IDENT("x".to_string()),
+        Token::PLUS,
+        Token::IDENT("y".to_string()),
+        Token::SEMICOLON,
+        Token::RBRACE,
+        Token::SEMICOLON,
+        Token::LET,
+        Token::IDENT("result".to_string()),
+        Token::ASSIGN,
+        Token::IDENT("add".to_string()),
+        Token::LPAREN,
+        Token::IDENT("x".to_string()),
+        Token::COMMA,
+        Token::IDENT("y".to_string()),
+        Token::RPAREN,
+        Token::SEMICOLON,
+        Token::EOF,
+    ];
+    assert_eq!(expected.len(), tokens.len());
+    for i in 0..expected.len() {
+        assert_eq!(expected[i], tokens[i]);
+    }
+}
+
